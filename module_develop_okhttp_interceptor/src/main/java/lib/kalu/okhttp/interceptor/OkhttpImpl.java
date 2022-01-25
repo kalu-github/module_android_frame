bee688d3cb545b4768939e05941b54c63c2f0714
kalu <153437803@qq.com>
2022-01-25 16:35:31 +0800

    update api

---
+++ module_develop_okhttp_interceptor/src/main/java/lib/kalu/okhttp/interceptor/OkhttpImpl.java
@@ -0,0 +1,285 @@
+package lib.kalu.okhttp.interceptor;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.Map;
+
+import lib.kalu.okhttp.interceptor.util.OkhttpUtil;
+import okhttp3.Connection;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.Buffer;
+
+/**
+ * description: 常量标记
+ * created by kalu on 2021-07-31
+ */
+interface OkhttpImpl {
+
+    /**
+     * 无需加密
+     */
+    String HttpAes = "xAes:1";
+    /**
+     * 需要缓存
+     */
+    String HttpCached = "xCached:1";
+    /**
+     * 需要登录公共参数
+     */
+    String HttpLogin = "xLogin:1";
+    /**
+     * 下载文件
+     */
+    String HttpDownload = "xDownload:1";
+    /**
+     * 流文件
+     */
+    String HttpMupdf = "xStream:1";
+
+    /**
+     * text/html
+     */
+    String TEXT_HTML = "text/html";
+    /**
+     * text/plain;
+     */
+    String TEXT_PLAIN = "text/plain";
+    /**
+     * application/json
+     */
+    String APPLICATION_JSON = "application/json";
+    /**
+     * "application/x-www-form-urlencoded"
+     */
+    String APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+    /**
+     * application/json;charset=utf-8
+     */
+    String APPLICATION_JSON_CHARSET_UTF8 = "application/json;charset=utf-8";
+    /**
+     * "
+     */
+    String SLASH = "\"";
+    /**
+     * ":"
+     */
+    String COLON = "\":\"";
+    /**
+     * ,
+     */
+    String COMMA = ",";
+    /**
+     * {
+     */
+    String BRACES_LEFT = "{";
+    /**
+     * }
+     */
+    String BRACES_RIGHT = "}";
+    /**
+     * content
+     */
+    String CONTENT = "content";
+    /**
+     * session
+     */
+    String SESSION = "session";
+    /**
+     * key
+     */
+    String KEY = "key";
+    /**
+     * extra
+     */
+    String EXTRA = "extra";
+    /**
+     * data
+     */
+    String DATA = "data";
+    /**
+     * code
+     */
+    String CODE = "code";
+    /**
+     * code
+     */
+    String CODE_DEDAULT = "1000";
+    /**
+     * message
+     */
+    String MESSAGE = "message";
+    /**
+     * message
+     */
+    String MESSAGE_DEFAULT = "网络异常, 请稍后重试";
+
+    Request analysisRequest(
+            @NonNull long requestTime,
+            @NonNull Connection connection,
+            @NonNull Request request);
+
+    Response analysisResponse(
+            @NonNull long requestTime,
+            @NonNull Request request,
+            @NonNull Response response);
+
+    /********************************/
+
+    /********************************/
+
+    default boolean enableLogs() {
+        return OkhttpUtil.isPrint();
+    }
+
+    /********************************/
+
+    default void logsConnection(@NonNull long requestTime, @NonNull Connection connection) {
+
+        if (enableLogs() && null != connection) {
+            try {
+                logs("request[" + requestTime + "] => port = " + connection.protocol());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => port = null");
+            }
+            try {
+                logs("request[" + requestTime + "] => route = " + connection.route());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => route = null");
+            }
+            try {
+                logs("request[" + requestTime + "] => socket = " + connection.socket());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => socket = null");
+            }
+        }
+    }
+
+    default void logsRequest(@NonNull long requestTime, @NonNull Request request) {
+
+        if (enableLogs() && null != request) {
+            logs("************************************************************************");
+            try {
+                logs("request[" + requestTime + "] => url = " + request.url().toString());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => url = null");
+            }
+            try {
+                logs("request[" + requestTime + "] => host = " + request.url().host());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => host = null");
+            }
+            try {
+                logs("request[" + requestTime + "] => method = " + request.method());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => method = null");
+            }
+            try {
+                logs("request[" + requestTime + "] => contentType = " + request.body().contentType());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => contentType = null");
+            }
+            try {
+                logs("request[" + requestTime + "] => contentLength = " + request.body().contentLength());
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => contentLength = null");
+            }
+        }
+    }
+
+    default void logsResponse(@NonNull long requestTime, @NonNull Response response) {
+        if (enableLogs() && null != response) {
+            logs("------------------------------------------------------------------------");
+            try {
+                long responseTime = System.nanoTime();
+                long time = Math.abs(responseTime - requestTime) / 1000000;
+                logs("response[" + requestTime + "] => time = " + time + "ms");
+            } catch (Exception e) {
+                logs("response[" + requestTime + "] => time = null");
+            }
+            try {
+                logs("response[" + requestTime + "] => code = " + response.code());
+            } catch (Exception e) {
+                logs("response[" + requestTime + "] => code = null");
+            }
+            try {
+                logs("response[" + requestTime + "] => message = " + response.message());
+            } catch (Exception e) {
+                logs("response[" + requestTime + "] => message = null");
+            }
+            try {
+                logs("response[" + requestTime + "] => contentType = " + response.body().contentType());
+            } catch (Exception e) {
+                logs("response[" + requestTime + "] => contentType = null");
+            }
+            try {
+                logs("response[" + requestTime + "] => contentLength = " + response.body().contentLength());
+            } catch (Exception e) {
+                logs("response[" + requestTime + "] => contentLength = null");
+            }
+        }
+    }
+
+    default void logsRequestHeaders(@NonNull long requestTime, @NonNull Headers headers) {
+
+        if (enableLogs() && null != headers) {
+            Map<String, List<String>> map = headers.toMultimap();
+            String head = String.valueOf(map);
+            logs("request[" + requestTime + "] => head = " + head);
+        }
+    }
+
+    default void logsResponseHeaders(@NonNull long requestTime, @NonNull Headers headers) {
+
+        if (enableLogs() && null != headers) {
+            Map<String, List<String>> map = headers.toMultimap();
+            String head = String.valueOf(map);
+            logs("response[" + requestTime + "] => head = " + head);
+            logs("************************************************************************");
+        }
+    }
+
+    default void logsResponseBody(@NonNull long requestTime, @NonNull String body) {
+        if (enableLogs() && null != body) {
+            logs("response[" + requestTime + "] => body = " + body);
+        }
+    }
+
+    default void logsRequestBody(@NonNull long requestTime, @NonNull String body) {
+        if (enableLogs() && null != body) {
+            logs("request[" + requestTime + "] => body = " + body);
+        }
+    }
+
+    default void logsRequestBody(@NonNull long requestTime, @NonNull FormBody formBody) {
+        if (enableLogs() && null != formBody) {
+            try {
+                Buffer buffer = new Buffer();
+                formBody.writeTo(buffer);
+                String readUtf8 = buffer.readUtf8();
+                logs("request[" + requestTime + "] => body = " + readUtf8);
+            } catch (Exception e) {
+                logs("request[" + requestTime + "] => body = " + e.getMessage());
+            }
+        }
+    }
+
+    default void logs(String message) {
+
+        if (enableLogs()) {
+            logs(message, null);
+        }
+    }
+
+    default void logs(String message, Throwable throwable) {
+
+        if (enableLogs()) {
+            OkhttpUtil.logE(message, throwable);
+        }
+    }
+
+    /********************************/
+}
---
+++ module_develop_okhttp_interceptor/src/main/java/lib/kalu/okhttp/interceptor/OkhttpInterceptor.java
@@ -0,0 +1,169 @@
+package lib.kalu.okhttp.interceptor;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import okhttp3.Connection;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * description: 网络拦截器
+ * created by kalu on 2021-07-31
+ */
+interface OkhttpInterceptor extends Interceptor, OkhttpImpl {
+
+    /**
+     * 拦截器入口
+     *
+     * @param chain
+     * @return
+     */
+    @NonNull
+    @Override
+    default Response intercept(@NonNull Chain chain) {
+        long requestTime = System.nanoTime();
+        try {
+            Connection connection = chain.connection();
+            Request request = chain.request();
+            Request newRequest = analysisRequest(requestTime, connection, request);
+            Response response = chain.proceed(newRequest);
+            Response newResponse = analysisResponse(requestTime, newRequest, response);
+            return newResponse;
+        } catch (Exception e) {
+            HashMap<CharSequence, CharSequence> map = new HashMap<>();
+            map.put(CODE, CODE_DEDAULT);
+            map.put(MESSAGE, MESSAGE_DEFAULT);
+            Request request = chain.request();
+            Response response = createResponse(request, map);
+            Response newResponse = analysisResponse(requestTime, request, response);
+            return newResponse;
+        }
+    }
+
+    /**
+     * 默认响应报文
+     *
+     * @param request
+     * @param map
+     * @return
+     */
+    default Response createResponse(@NonNull Request request, @NonNull Map<CharSequence, CharSequence> map) {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(BRACES_LEFT);
+
+        if (null != map && map.size() > 0) {
+            Iterator<Map.Entry<CharSequence, CharSequence>> iterator = map.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<CharSequence, CharSequence> next = iterator.next();
+                CharSequence key = next.getKey();
+                if (null == key || key.length() == 0)
+                    continue;
+                CharSequence value = next.getValue();
+                builder.append(SLASH);
+                builder.append(key);
+                builder.append(COLON);
+                builder.append(value);
+                builder.append(SLASH);
+                if (!iterator.hasNext())
+                    continue;
+                builder.append(COMMA);
+            }
+        }
+        builder.append(BRACES_RIGHT);
+
+        String text = builder.toString();
+        ResponseBody responseBody = ResponseBody.create(MediaType.parse(APPLICATION_JSON_CHARSET_UTF8), text);
+
+        Response response = new Response.Builder()
+                .code(200)
+                .protocol(Protocol.HTTP_1_1)
+                .message(MESSAGE_DEFAULT)
+                .request(request)
+                .body(responseBody)
+                .build();
+
+        return response;
+    }
+
+    /**
+     * 处理加工 => 请求报文
+     *
+     * @param text
+     * @param headersBuilder
+     * @return
+     */
+    default String processRequest(@NonNull String text, @NonNull Headers.Builder headersBuilder) {
+        try {
+            JSONObject object = new JSONObject(text);
+            String session = object.optString(SESSION, null);
+            object.remove(SESSION);
+            headersBuilder.add(SESSION, session);
+            return object.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * 处理加工 => 请求报文
+     *
+     * @param formBody
+     * @param headersBuilder
+     * @return
+     */
+    default FormBody processRequest(@NonNull FormBody formBody, @NonNull Headers.Builder headersBuilder) {
+        try {
+            FormBody.Builder builder = new FormBody.Builder();
+            for (int i = 0; i < formBody.size(); i++) {
+                String key = formBody.encodedName(i);
+                if (null == key || key.length() == 0)
+                    continue;
+                String value = formBody.encodedValue(i);
+                if (SESSION.equals(key)) {
+                    headersBuilder.add(key, value);
+                } else {
+                    builder.add(key, value);
+                }
+            }
+            return builder.build();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * 处理加工 => 响应报文
+     *
+     * @param text
+     * @param request
+     * @return
+     */
+    default String processResponse(@NonNull String text, @NonNull Request request) {
+        try {
+            JSONObject object = new JSONObject(text);
+            String parameter = request.url().queryParameter(EXTRA);
+            String extra = request.headers().get(EXTRA);
+            if (null != parameter && parameter.length() > 0) {
+                object.putOpt(EXTRA, parameter);
+            } else if (null != extra && extra.length() > 0) {
+                object.putOpt(EXTRA, extra);
+            }
+            return object.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
