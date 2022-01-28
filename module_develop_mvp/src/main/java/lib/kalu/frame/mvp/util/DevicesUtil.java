943f49d4a7dafc06724986bceff50622ec31f926
kalu <153437803@qq.com>
2022-01-28 12:58:35 +0800

    update api

---
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/util/DevicesUtil.java
@@ -0,0 +1,322 @@
+package lib.kalu.frame.mvp.util;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Locale;
+
+public class DevicesUtil {
+
+    /**
+     * 获取当前手机系统语言。
+     *
+     * @return 返回当前系统语言。例如：当前设置的是“中文-中国”，则返回“zh-CN”
+     */
+    public static String getSystemLanguage() {
+        return Locale.getDefault().getLanguage();
+    }
+
+    /**
+     * 获取当前系统上的语言列表(Locale列表)
+     *
+     * @return 语言列表
+     */
+    public static Locale[] getSystemLanguageList() {
+        return Locale.getAvailableLocales();
+    }
+
+    /**
+     * 获取当前手机系统版本号
+     *
+     * @return 系统版本号
+     */
+    public static String getSystemVersion() {
+        return Build.VERSION.RELEASE;
+    }
+
+    /**
+     * 获取设备型号
+     *
+     * @return 设备型号
+     */
+    public static String getSystemModel() {
+        return Build.MODEL;
+    }
+
+    /**
+     * 获取设备厂商
+     *
+     * @return 设备厂商
+     */
+    public static String getDeviceBrand() {
+        return Build.BRAND;
+    }
+
+
+    /**
+     * 获取MAC地址
+     *
+     * @param context
+     * @return
+     */
+    public static String getMacAddress(Context context) {
+        String mac = getMacDefault(context);
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac)) {
+            Log.e("mac", "mac method a is null");
+            mac = getMacFromFile();
+        }
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac)) {
+            Log.e("mac", "mac method b is null");
+            mac = getMacFromFile2();
+        }
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac)) {
+            Log.e("mac", "mac method c is null");
+            mac = getMacFromHardware();
+        }
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac)) {
+            Log.e("mac", "mac method e is null");
+            mac = getMacFromHardware2();
+        }
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac))
+            mac = getLocalMacAddressFromIp();
+        if (TextUtils.isEmpty(mac) || "02:00:00:00:00:00".equals(mac))
+            mac = "";
+        if (mac != null)
+            mac = mac.toUpperCase(Locale.ENGLISH);
+        return mac;
+    }
+
+    /**
+     * Android  6.0 之前（不包括6.0）
+     * 必须的权限  <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+     *
+     * @param context
+     * @return
+     */
+    @SuppressLint("HardwareIds")
+    private static String getMacDefault(Context context) {
+        String mac = "";
+        if (context == null) {
+            return mac;
+        }
+
+        WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifi == null) {
+            return mac;
+        }
+        WifiInfo info = null;
+        try {
+            info = wifi.getConnectionInfo();
+        } catch (Exception ignored) {
+        }
+        if (info == null) {
+            return null;
+        }
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return mac;
+        }
+        mac = info.getMacAddress();
+        if (!TextUtils.isEmpty(mac)) {
+            mac = mac.toUpperCase(Locale.ENGLISH);
+        }
+        return mac;
+    }
+
+    /**
+     * Android 6.0（包括） - Android 7.0（不包括）
+     *
+     * @return
+     */
+    private static String getMacFromFile() {
+        String WifiAddress = "";
+        try {
+            WifiAddress = new BufferedReader(new FileReader(new File("/sys/class/net/wlan0/address"))).readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return WifiAddress;
+    }
+
+    private static String getMacFromFile2() {
+        String WifiAddress = "";
+        try {
+            WifiAddress = new BufferedReader(new FileReader(new File("/sys/class/net/eth0/address"))).readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return WifiAddress;
+    }
+
+    /**
+     * 遍历循环所有的网络接口，找到接口是 wlan0
+     * 必须的权限 <uses-permission android:name="android.permission.INTERNET" />
+     *
+     * @return
+     */
+    public static String getMacFromHardware() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String getMacFromHardware2() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("eth0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 根据IP地址获取MAC地址
+     *
+     * @return
+     */
+    private static String getLocalMacAddressFromIp() {
+        String strMacAddr = null;
+        try {
+            //获得IpD地址
+            InetAddress ip = getLocalInetAddress();
+            byte[] b = NetworkInterface.getByInetAddress(ip).getHardwareAddress();
+            StringBuilder buffer = new StringBuilder();
+            for (int i = 0; i < b.length; i++) {
+                if (i != 0) {
+                    buffer.append(':');
+                }
+                String str = Integer.toHexString(b[i] & 0xFF);
+                buffer.append(str.length() == 1 ? 0 + str : str);
+            }
+            strMacAddr = buffer.toString().toUpperCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (TextUtils.isEmpty(strMacAddr))
+            strMacAddr = "";
+        return strMacAddr;
+    }
+
+    /**
+     * 获取移动设备本地IP
+     *
+     * @return
+     */
+    public static InetAddress getLocalInetAddress() {
+        InetAddress ip = null;
+        try {
+            //列举
+            Enumeration<NetworkInterface> en_netInterface = NetworkInterface.getNetworkInterfaces();
+            while (en_netInterface.hasMoreElements()) {//是否还有元素
+                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();//得到下一个元素
+                Enumeration<InetAddress> en_ip = ni.getInetAddresses();//得到一个ip地址的列举
+                while (en_ip.hasMoreElements()) {
+                    ip = en_ip.nextElement();
+                    if (!ip.isLoopbackAddress() && !ip.getHostAddress().contains(":"))
+                        break;
+                    else
+                        ip = null;
+                }
+
+                if (ip != null) {
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+
+            e.printStackTrace();
+        }
+        return ip;
+    }
+
+    public static String getIp() {
+        InetAddress inetAddress = getLocalInetAddress();
+        if (inetAddress == null)
+            return "0.0.0.0";
+        String ip = inetAddress.getHostAddress();
+        if (TextUtils.isEmpty(ip))
+            ip = "0.0.0.0";
+        return ip;
+    }
+
+    /**
+     * 获取设备机型
+     *
+     * @return x
+     */
+    public static String getDeviceModel() {
+        String model = "";
+//        try {
+//            MiscManager miscManager = TVManager.getInstance(context).getMiscManager();
+//            model = miscManager.getCurProductName();
+//        } catch (Exception e) {
+////            e.printStackTrace();
+//        }
+        if (TextUtils.isEmpty(model))
+            model = Build.MODEL;
+        return model;
+    }
+}
