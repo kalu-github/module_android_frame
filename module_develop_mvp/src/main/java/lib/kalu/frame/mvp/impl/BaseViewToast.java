2a63ba0a91419cb40982e0b6378e6301bb7d7399
kalu <153437803@qq.com>
2023-03-08 12:58:54 +0800

    update api

--- module_develop_mvp/src/main/java/lib/kalu/frame/mvp/impl/BaseViewToast.java
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/impl/BaseViewToast.java
@@ -9,16 +9,28 @@ import android.widget.Toast;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
+import lib.kalu.frame.mvp.util.MvpUtil;
+
 @Keep
 public interface BaseViewToast extends BaseViewContext {
 
+    default void showToast(@NonNull java.lang.Throwable throwable) {
+        try {
+            String message = throwable.getMessage();
+            showToast(message);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewToast => showToast => " + e.getMessage());
+        }
+    }
+
     default void showToast(@NonNull String s) {
         try {
-            if (null != s && s.length() > 0) {
-                Context context = getContext();
-                Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
-            }
+            if (null == s || s.length() >= 0)
+                throw new Exception("message is null");
+            Context context = getContext();
+            Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
+            MvpUtil.logE("BaseViewToast => showToast => " + e.getMessage());
         }
     }
 }
