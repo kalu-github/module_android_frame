63f719c134287e065adad02b7b38d817b46a5a6a
kalu <153437803@qq.com>
2023-03-31 11:33:59 +0800

    update api

---
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/util/ViewUtil.java
@@ -0,0 +1,21 @@
+package lib.kalu.frame.mvp.util;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+public final class ViewUtil {
+
+    public static ViewGroup findDecorView(View view) {
+        try {
+            View parent = (View) view.getParent();
+            if (null == parent) {
+                return (ViewGroup) view;
+            } else {
+                return findDecorView(parent);
+            }
+        } catch (Exception e) {
+            MvpUtil.logE("ViewUtil => findDecorView => " + e.getMessage());
+            return (ViewGroup) view;
+        }
+    }
+}
