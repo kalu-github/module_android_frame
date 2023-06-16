98631e7e8e13a13c1860d56bb156af055609b425
kalu <153437803@qq.com>
2023-06-16 15:11:17 +0800

    update api

---
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/BaseDialogFragment.java
@@ -0,0 +1,25 @@
+﻿package lib.kalu.frame.mvp;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import lib.kalu.frame.mvp.util.MvpUtil;
+
+public class BaseDialogFragment extends DialogFragment {
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void setupDialog(@NonNull Dialog dialog, int style) {
+        try {
+            super.setupDialog(dialog, style);
+        }catch (Exception e){
+            MvpUtil.logE("BaseDialogFragment => setupDialog => " + e.getMessage());
+        }
+    }
+}
