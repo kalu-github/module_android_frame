d1e431a3ec3435d4c1ea842104ec74abf6e28abf
kalu <153437803@qq.com>
2023-06-16 15:15:13 +0800

    update api

--- module_develop_mvp/src/main/java/lib/kalu/frame/mvp/BaseDialogFragment.java
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/BaseDialogFragment.java
@@ -2,12 +2,17 @@
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.lang.reflect.Field;
 
 import lib.kalu.frame.mvp.util.MvpUtil;
 
@@ -20,6 +25,42 @@ public class BaseDialogFragment extends DialogFragment {
             super.setupDialog(dialog, style);
         }catch (Exception e){
             MvpUtil.logE("BaseDialogFragment => setupDialog => " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void show(@NonNull FragmentManager manager, @Nullable String tag) {
+        try {
+            Class cls = DialogFragment.class;
+            Field mDismissed = cls.getDeclaredField("mDismissed");
+            mDismissed.setAccessible(true);
+            mDismissed.set(this, false);
+            Field mShownByMe = cls.getDeclaredField("mShownByMe");
+            mShownByMe.setAccessible(true);
+            mShownByMe.set(this, true);
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(this, tag);
+            transaction.commitAllowingStateLoss();
+        } catch (Exception e) {
+            super.show(manager, tag);
+        }
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        try {
+            dismissAllowingStateLoss();
+        } catch (Exception e) {
+            super.onDismiss(dialog);
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        try {
+            dismissAllowingStateLoss();
+        } catch (Exception e) {
+            super.dismiss();
         }
     }
 }
