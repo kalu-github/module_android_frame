54b1fe3dbe562e680f8d47583252208636c81999
kalu <153437803@qq.com>
2023-08-07 18:42:57 +0800

    update files

---
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/util/ApkUtil.java
@@ -0,0 +1,64 @@
+package lib.kalu.frame.mvp.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+
+public final class ApkUtil {
+
+    private static String PACKAGE_ARCHIVE = "application/vnd.android.package-archive";
+    private static String PACKAGE = "package:";
+    private static String AUTHORITY = "lib.kalu.frame.mvp.common.upgrade";
+
+    public static boolean installApk(@NonNull Context context, @NonNull String apkPath) {
+        try {
+            if (null == context)
+                throw new Exception("context error: null");
+            if (null == apkPath || apkPath.length() == 0)
+                throw new Exception("apkPath error: " + apkPath);
+            File file = new File(apkPath);
+            if (!file.exists())
+                throw new Exception("apkFile error: not exists");
+            Intent intent = new Intent();
+            // 8.0
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                MvpUtil.logE("ApkUtil => installApk => android8.0");
+                boolean canRequestPackageInstalls = context.getPackageManager().canRequestPackageInstalls();
+                MvpUtil.logE("ApkUtil => installApk => android8.0 => canRequestPackageInstalls = " + canRequestPackageInstalls);
+                if (canRequestPackageInstalls) {
+                    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    Uri uri = FileProvider.getUriForFile(context, AUTHORITY, file);
+                    intent.setDataAndType(uri, PACKAGE_ARCHIVE);
+                } else {
+                    intent.setAction(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+                    intent.setData(Uri.parse(PACKAGE + context.getPackageName()));
+                }
+            }
+            // 7.0
+            else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                MvpUtil.logE("ApkUtil => installApk => android7.0");
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                Uri uri = FileProvider.getUriForFile(context, AUTHORITY, file);
+                intent.setDataAndType(uri, PACKAGE_ARCHIVE);
+            } else {
+                MvpUtil.logE("ApkUtil => installApk => android6.0");
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setDataAndType(Uri.fromFile(file), PACKAGE_ARCHIVE);
+            }
+            context.startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            MvpUtil.logE("ApkUtil => installApk => " + e.getMessage());
+            return false;
+        }
+    }
+}
