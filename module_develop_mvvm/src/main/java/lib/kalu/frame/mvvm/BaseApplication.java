9cde564cda9f60e837d56534415823b7b842cea2
kalu <153437803@qq.com>
2023-08-10 15:32:25 +0800

    update files

--- module_develop_mvvm/src/main/java/lib/kalu/frame/mvvm/BaseApplication.java
+++ module_develop_mvvm/src/main/java/lib/kalu/frame/mvvm/BaseApplication.java
@@ -3,7 +3,7 @@ package lib.kalu.frame.mvvm;
 import android.app.Application;
 import android.content.Context;
 
-import lib.kalu.frame.mvp.context.FrameContext;
+import lib.kalu.frame.mvvm.context.FrameContext;
 
 public class BaseApplication extends Application {
 
