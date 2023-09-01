7696b43f09644020690797dfb38bb969094e56b0
kalu <153437803@qq.com>
2023-09-01 12:15:00 +0800

    update files

--- module_develop_mvp/src/main/java/lib/kalu/frame/mvp/BaseApplication.java
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/BaseApplication.java
@@ -2,12 +2,32 @@ package lib.kalu.frame.mvp;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+import android.webkit.WebView;
 
 import lib.kalu.frame.mvp.context.FrameContext;
+import lib.kalu.frame.mvp.util.MvpUtil;
 
 public class BaseApplication extends Application {
 
     public static final Context getContext() {
         return FrameContext.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initWebViewProcess();
+    }
+
+    private void initWebViewProcess() {
+        try {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P)
+                throw new Exception("SDK_INT warning: not need");
+            String processName = getProcessName();
+            WebView.setDataDirectorySuffix(processName);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseApplication => initWebViewProcess => " + e.getMessage());
+        }
     }
 }
