af3e809b1c697a10a7f7b6fd0b2fa9ed9d19dbc7
kalu <153437803@qq.com>
2023-10-19 14:49:58 +0800

    update files

--- module_develop_mvp/src/main/java/lib/kalu/frame/mvp/impl/BaseViewRecyclerView.java
+++ module_develop_mvp/src/main/java/lib/kalu/frame/mvp/impl/BaseViewRecyclerView.java
@@ -1,11 +1,14 @@
 package lib.kalu.frame.mvp.impl;
 
+import android.view.View;
+
 import androidx.annotation.IdRes;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import lib.kalu.frame.mvp.BaseView;
+import lib.kalu.frame.mvp.util.MvpUtil;
 
 @Keep
 public interface BaseViewRecyclerView {
@@ -14,7 +17,16 @@ public interface BaseViewRecyclerView {
         try {
             return getAdapetItemCount(id) <= 0;
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => isAdapterEmpty => " + e.getMessage());
+            return true;
+        }
+    }
+
+    default boolean isAdapterEmpty(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            return getAdapetItemCount(viewGroup, id) <= 0;
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => isAdapterEmpty => " + e.getMessage());
             return true;
         }
     }
@@ -24,7 +36,17 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             return recyclerView.getAdapter().getItemCount();
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => getAdapetItemCount => " + e.getMessage());
+            return 0;
+        }
+    }
+
+    default int getAdapetItemCount(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            return recyclerView.getAdapter().getItemCount();
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => getAdapetItemCount => " + e.getMessage());
             return 0;
         }
     }
@@ -34,7 +56,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyDataSetChanged();
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyDataSetChanged => " + e.getMessage());
+        }
+    }
+
+    default void notifyDataSetChanged(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyDataSetChanged();
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyDataSetChanged => " + e.getMessage());
         }
     }
 
@@ -44,7 +75,17 @@ public interface BaseViewRecyclerView {
             int count = recyclerView.getAdapter().getItemCount();
             notifyItemRangeChanged(id, 0, count);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyDataRangeChanged => " + e.getMessage());
+        }
+    }
+
+    default void notifyDataRangeChanged(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            int count = recyclerView.getAdapter().getItemCount();
+            notifyItemRangeChanged(id, 0, count);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyDataRangeChanged => " + e.getMessage());
         }
     }
 
@@ -57,7 +98,20 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRangeChanged(positionStart, itemCount);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeChanged => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeChanged(@NonNull View viewGroup, @IdRes int id, @NonNull int position) {
+        notifyItemRangeChanged(viewGroup, id, position, 1);
+    }
+
+    default void notifyItemRangeChanged(@NonNull View viewGroup, @IdRes int id, int positionStart, int itemCount) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyItemRangeChanged(positionStart, itemCount);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeChanged => " + e.getMessage());
         }
     }
 
@@ -66,7 +120,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRangeRemoved(position, count);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeRemoved => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeRemoved(@NonNull View viewGroup, @IdRes int id, @NonNull int position, @NonNull int count) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyItemRangeRemoved(position, count);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeRemoved => " + e.getMessage());
         }
     }
 
@@ -79,7 +142,20 @@ public interface BaseViewRecyclerView {
             int itemCount = adapter.getItemCount();
             recyclerView.getAdapter().notifyItemRangeRemoved(0, itemCount);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeRemovedAll => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeRemovedAll(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+            if (null == adapter)
+                throw new Exception("adapter error: null");
+            int itemCount = adapter.getItemCount();
+            recyclerView.getAdapter().notifyItemRangeRemoved(0, itemCount);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeRemovedAll => " + e.getMessage());
         }
     }
 
@@ -88,7 +164,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRemoved(position);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRemoved => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRemoved(@NonNull View viewGroup, @IdRes int id, @NonNull int position) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyItemRemoved(position);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRemoved => " + e.getMessage());
         }
     }
 
@@ -97,7 +182,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRangeInserted(position, length);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeInserted => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeInserted(@NonNull View viewGroup, @IdRes int id, @NonNull int position, int length) {
+        try {
+            RecyclerView recyclerView = ((BaseView) this).findViewById(id);
+            recyclerView.getAdapter().notifyItemRangeInserted(position, length);
+        } catch (Exception e) {
+            MvpUtil.logE("BaseViewRecyclerView => notifyItemRangeInserted => " + e.getMessage());
         }
     }
 }
--- module_develop_mvvm/src/main/java/lib/kalu/frame/mvvm/impl/BaseViewRecyclerView.java
+++ module_develop_mvvm/src/main/java/lib/kalu/frame/mvvm/impl/BaseViewRecyclerView.java
@@ -1,11 +1,14 @@
 package lib.kalu.frame.mvvm.impl;
 
+import android.view.View;
+
 import androidx.annotation.IdRes;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import lib.kalu.frame.mvvm.BaseView;
+import lib.kalu.frame.mvvm.util.MvvmUtil;
 
 @Keep
 public interface BaseViewRecyclerView {
@@ -14,7 +17,16 @@ public interface BaseViewRecyclerView {
         try {
             return getAdapetItemCount(id) <= 0;
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => isAdapterEmpty => " + e.getMessage());
+            return true;
+        }
+    }
+
+    default boolean isAdapterEmpty(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            return getAdapetItemCount(viewGroup, id) <= 0;
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => isAdapterEmpty => " + e.getMessage());
             return true;
         }
     }
@@ -24,7 +36,17 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             return recyclerView.getAdapter().getItemCount();
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => getAdapetItemCount => " + e.getMessage());
+            return 0;
+        }
+    }
+
+    default int getAdapetItemCount(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            return recyclerView.getAdapter().getItemCount();
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => getAdapetItemCount => " + e.getMessage());
             return 0;
         }
     }
@@ -34,7 +56,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyDataSetChanged();
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyDataSetChanged => " + e.getMessage());
+        }
+    }
+
+    default void notifyDataSetChanged(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyDataSetChanged();
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyDataSetChanged => " + e.getMessage());
         }
     }
 
@@ -44,7 +75,17 @@ public interface BaseViewRecyclerView {
             int count = recyclerView.getAdapter().getItemCount();
             notifyItemRangeChanged(id, 0, count);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyDataRangeChanged => " + e.getMessage());
+        }
+    }
+
+    default void notifyDataRangeChanged(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            int count = recyclerView.getAdapter().getItemCount();
+            notifyItemRangeChanged(id, 0, count);
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyDataRangeChanged => " + e.getMessage());
         }
     }
 
@@ -57,7 +98,20 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRangeChanged(positionStart, itemCount);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeChanged => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeChanged(@NonNull View viewGroup, @IdRes int id, @NonNull int position) {
+        notifyItemRangeChanged(viewGroup, id, position, 1);
+    }
+
+    default void notifyItemRangeChanged(@NonNull View viewGroup, @IdRes int id, int positionStart, int itemCount) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyItemRangeChanged(positionStart, itemCount);
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeChanged => " + e.getMessage());
         }
     }
 
@@ -66,7 +120,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRangeRemoved(position, count);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeRemoved => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeRemoved(@NonNull View viewGroup, @IdRes int id, @NonNull int position, @NonNull int count) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyItemRangeRemoved(position, count);
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeRemoved => " + e.getMessage());
         }
     }
 
@@ -79,7 +142,20 @@ public interface BaseViewRecyclerView {
             int itemCount = adapter.getItemCount();
             recyclerView.getAdapter().notifyItemRangeRemoved(0, itemCount);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeRemovedAll => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeRemovedAll(@NonNull View viewGroup, @IdRes int id) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            RecyclerView.Adapter adapter = recyclerView.getAdapter();
+            if (null == adapter)
+                throw new Exception("adapter error: null");
+            int itemCount = adapter.getItemCount();
+            recyclerView.getAdapter().notifyItemRangeRemoved(0, itemCount);
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeRemovedAll => " + e.getMessage());
         }
     }
 
@@ -88,7 +164,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRemoved(position);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRemoved => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRemoved(@NonNull View viewGroup, @IdRes int id, @NonNull int position) {
+        try {
+            RecyclerView recyclerView = viewGroup.findViewById(id);
+            recyclerView.getAdapter().notifyItemRemoved(position);
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRemoved => " + e.getMessage());
         }
     }
 
@@ -97,7 +182,16 @@ public interface BaseViewRecyclerView {
             RecyclerView recyclerView = ((BaseView) this).findViewById(id);
             recyclerView.getAdapter().notifyItemRangeInserted(position, length);
         } catch (Exception e) {
-            e.printStackTrace();
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeInserted => " + e.getMessage());
+        }
+    }
+
+    default void notifyItemRangeInserted(@NonNull View viewGroup, @IdRes int id, @NonNull int position, int length) {
+        try {
+            RecyclerView recyclerView = ((BaseView) this).findViewById(id);
+            recyclerView.getAdapter().notifyItemRangeInserted(position, length);
+        } catch (Exception e) {
+            MvvmUtil.logE("BaseViewRecyclerView => notifyItemRangeInserted => " + e.getMessage());
         }
     }
 }
