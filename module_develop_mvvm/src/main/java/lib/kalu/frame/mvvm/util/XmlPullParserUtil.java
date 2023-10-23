af87397957e45839b90b9ac3ec92f5fb522880ea
kalu <153437803@qq.com>
2023-10-23 17:16:34 +0800

    update files

---
+++ module_develop_mvvm/src/main/java/lib/kalu/frame/mvvm/util/XmlPullParserUtil.java
@@ -0,0 +1,55 @@
+package lib.kalu.frame.mvvm.util;
+
+import androidx.annotation.NonNull;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+
+public class XmlPullParserUtil {
+
+    public static String parser(@NonNull String data, @NonNull String nodeName, @NonNull String keyName) {
+        try {
+            if (null == data || data.length() == 0)
+                throw new Exception("data error: " + data);
+            if (null == nodeName || nodeName.length() == 0)
+                throw new Exception("nodeName error: " + nodeName);
+            if (null == keyName || keyName.length() == 0)
+                throw new Exception("keyName error: " + keyName);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(data));
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                try {
+                    String name = parser.getName();
+                    if (null == name || name.length() == 0)
+                        throw new Exception("name warning: " + name);
+                    MvvmUtil.logE("XmlPullParserUtil => parser => noteName = " + name);
+                    switch (eventType) {
+                        case XmlPullParser.START_TAG://开始解析
+                            if (!name.equals(nodeName))
+                                throw new Exception("name warning: not equals " + name);
+                            String value = parser.getAttributeValue(null, keyName);
+                            MvvmUtil.logE("XmlPullParserUtil => parser => noteName = " + name + ", keyName = " + keyName + ", value = " + value);
+                            if (null == value || value.length() == 0){
+                                return null;
+                            }
+                            else{
+                                return value;
+                            }
+                    }
+                    throw new Exception("not find");
+                } catch (Exception e) {
+                    MvvmUtil.logE("XmlPullParserUtil => parser => " + e.getMessage());
+                    eventType = parser.next();
+                }
+            }
+            throw new Exception("not find");
+        } catch (Exception e) {
+            MvvmUtil.logE("XmlPullParserUtil => parser => " + e.getMessage());
+            return null;
+        }
+    }
+}
